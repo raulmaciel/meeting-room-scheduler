f5be4b4d0580a17488de60f539cf3a8a048c8587
raulmaciel <raulmacielxd@gmail.com>
2026-02-18 20:12:06 -0300

    first commit

---
+++ src/main/java/dev/raul/meeting_room_scheduler/MeetingRoomSchedulerApplication.java
@@ -0,0 +1,13 @@
+package dev.raul.meeting_room_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/raul/meeting_room_scheduler/MeetingRoomSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raul.meeting_room_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
